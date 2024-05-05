93bc9d4d85ccb72ebab88dcbca8ad61d6187bd2e
kwonsunny <kwonsunny@naver.com>
2024-05-05 21:39:08 +0900

    5.5

--- src/main/java/pay/persistence/CouponDAO.java
+++ src/main/java/pay/persistence/CouponDAO.java
@@ -1,5 +1,6 @@
 package pay.persistence;
 
 public interface CouponDAO {
-		public abstract int issue(String id , int couponnum) ;
+	public abstract int issue(String id , int couponnum) ;
+	public abstract int isduplicoupon(String id , int couponnum) ;
 }
--- src/main/java/pay/persistence/CouponImpl.java
+++ src/main/java/pay/persistence/CouponImpl.java
@@ -48,6 +48,35 @@ public class CouponImpl implements CouponDAO{
 		return a;
 	}
 
+	@Override
+	public int isduplicoupon(String id, int couponnum) {
+		String sql = " select count(*) result from couponrecord where memid= ? and cnumber= ? ";
+		int result = 0 ; 
+		try {
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, id);
+			pst.setInt(2, couponnum);
+			rs = pst.executeQuery();
+			if (rs.next()) {
+				result = rs.getInt("result");
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		return result;
+	}
+
 	
 
 }
