5041fa2163e95391e47b7d0fd92862b8a72cf71e
daetu <daetu01@gmail.com>
2024-05-07 17:06:21 +0900

    userinfo page login

--- src/main/java/member/command/LoginHandler.java
+++ src/main/java/member/command/LoginHandler.java
@@ -29,13 +29,24 @@ public class LoginHandler implements CommandHandler {
 		// post , get 그리고 get에서도 뒤에 쿼리 스트링이 오는 친구냐에 따라. 그리고 그 타입에 따라서 로그인 분기점을 정해줘야함. 
 		String contextPath = request.getContextPath();
 		String method = request.getMethod();
+		System.out.println(method);
 		if ( method.equals("GET")) {
 					
 			// GET 요청으로 주소 돌려주기 
-			String path = "/member/loginPage_new_Form.jsp";
-			return path;
 			
 			
+			// servletPath << 이거 SSGSSAK 뒤의 값을 돌려줌 . 
+			String servletPath = request.getServletPath();
+			String path = "";
+			System.out.println("1 " + servletPath);
+			if ( servletPath.equals("/member/login/popup.do")) {
+				 path = "/member/loginPage_new_Form.jsp";
+				 return path;			 
+			} else {	
+				path = contextPath + "/member/login_Full.jsp";				
+				response.sendRedirect(path);
+			}
+				
 		} else {
 			// P	OST 요청 
 			
@@ -57,7 +68,8 @@ public class LoginHandler implements CommandHandler {
 			loginMember = service.idcheck(id, passwd);
 			
 			if ( loginMember == null) {
-				return null; // ajax 에서 처리 
+				String redirectPath = contextPath + request.getServletPath();
+				response.sendRedirect(redirectPath);
 			}
 			
 			HttpSession session = request.getSession();
@@ -68,10 +80,11 @@ public class LoginHandler implements CommandHandler {
 			 String path = "/member/login.jsp";
 			 
 			
-			 RequestDispatcher dispatcher =
-					 request.getRequestDispatcher(path); 
-			 dispatcher.forward(request, response);
-			 
+				
+				 RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+				 dispatcher.forward(request, response);
+				 
+			 response.sendRedirect(path);
 			 
 			 //session.setAttribute("auth",loginMember);
 			// 정확히는 쿠키에 저장하는 거임 .
--- src/main/webapp/member/login.jsp
+++ src/main/webapp/member/login.jsp
@@ -3,9 +3,7 @@
     pageEncoding="UTF-8"%>
 <%
 	
-	String id = (String)session.getAttribute("auth");
-    
-  
+	String id = (String)session.getAttribute("auth");      
 	String result = String.format("{" + "\"id\":\"%s\"" + "}",id);
 	System.out.print(result);
 %>
