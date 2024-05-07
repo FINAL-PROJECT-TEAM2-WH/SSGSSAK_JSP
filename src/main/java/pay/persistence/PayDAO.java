25a65a39abd6ed0b02b92e7b6a6eb39b44a16ce7
kwonsunny <kwonsunny@naver.com>
2024-05-08 00:14:26 +0900

    5.8

--- src/main/java/pay/persistence/PayDAO.java
+++ src/main/java/pay/persistence/PayDAO.java
@@ -1,7 +1,11 @@
 package pay.persistence;
 
+import java.util.ArrayList;
+
+import pay.domain.ProductDTO;
+
 public interface PayDAO {
 	public abstract int issue(String id , int couponnum) ;
 	public abstract int isduplicoupon(String id , int couponnum) ;
-	public abstract int viewpay(String productid , int option , int count) ;
+	public abstract ArrayList<ProductDTO> viewpay(String productid , int option ,int count)) ;
 }
