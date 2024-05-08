32dce7a5301a6ed2ae9cc8964cf7ee584cb0fbef
gun <kang930821@gmail.com>
2024-05-08 16:56:05 +0900

    올려올려

---
+++ src/main/java/productList/domain/CategoryDTO.java
@@ -0,0 +1,23 @@
+package productList.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryDTO {
+
+	private String id;
+	private String majorCateName;      
+	private String middleCateName;    
+	private String subCateName;   
+	private String miniCateName;   
+	
+	
+}
