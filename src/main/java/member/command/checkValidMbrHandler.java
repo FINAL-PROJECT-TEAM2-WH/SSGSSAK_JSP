2eef98af9b3263cd452a22da3fbd2f65eb4c2be7
daetu01 <daetu01@gmail.com>
2024-05-19 01:28:14 +0900

    add least option

--- src/main/java/member/command/checkValidMbrHandler.java
+++ src/main/java/member/command/checkValidMbrHandler.java
@@ -1,5 +1,7 @@
 package member.command;
 
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,10 +12,27 @@ public class checkValidMbrHandler implements CommandHandler {
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
+		String method = request.getMethod();
+		
 		
 		String contextPath = request.getContextPath();
 		String path = contextPath + "/member/checkMbr.jsp";
-		response.sendRedirect(path);
+		
+		Enumeration<String> enumeration = request.getHeaderNames();
+		
+		while (enumeration.hasMoreElements()) {
+			String string = enumeration.nextElement();
+			System.out.printf("key : %s || Value : %s ", string, request.getHeader(string));
+		}
+		if (method.equalsIgnoreCase("GET")) {
+			// 아이디 체크 GET 요청 => 체크 사이트로 리턴.  
+			response.sendRedirect(path);
+		} else {
+			// 아이디 체크할 값 바탕으로 체크하는 핸들러 거쳐서. 보여주기 . 
+			
+		}
+		
+		
 		
 		return null;
 	}
